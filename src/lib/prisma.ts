e5ed2fdc62509b0d22dc420eb4050882d0ac0517
HabitSpring <habitspring@gmail.com>
2025-12-04 10:59:30 +0530

    prisma fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,14 @@
-// src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client/edge';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL 
-});
+const adapter = new PrismaPg(connectionString);
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({ adapter });
+const prisma = globalThis.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+if (!globalThis.prisma) {
+  globalThis.prisma = prisma;
 }
+
+export default prisma;
