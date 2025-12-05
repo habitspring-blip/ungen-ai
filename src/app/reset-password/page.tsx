b2744db6f24570aa2079090d0425620946c23175
HabitSpring <habitspring@gmail.com>
2025-12-05 17:10:53 +0530

    Fix 2

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = false;
+
 
 import { useState, Suspense, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
