539f39ce4951fef87266ec5a93db5ae0a3f706ed
HabitSpring <habitspring@gmail.com>
2025-12-05 17:32:09 +0530

    Fix 5

--- src/app/reset-password/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-export const dynamic = "force-dynamic";
-export const revalidate = false;
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return children;
-}
